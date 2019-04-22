c151ec5e1db6353090d16fffa4dffca2e945cdc1
Ludophilia <c1q4n93@outlook.com>
2019-04-22 15:26:24 +0200

    Ajout de la fonctionnalité d'envoi de messages au backend. L'utilisateur peut envoyer un message simple et GrandPy en abckend lui répondra

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,7 +2,20 @@ var site_brand = document.getElementById("site_brand");
 var chat_input = document.getElementById("chat_input");
 var form = document.querySelector("form");
 var chat_zone = document.getElementById("chat_zone");
-var count = 0;
+var count_you = 0;
+var count_gp = -1;
+
+function ajaxTest(method, target, value, callback) {
+    var request = new XMLHttpRequest(); 
+    request.open(method, target);
+    request.addEventListener("load", function(){
+        var response = request.responseText;
+        console.log(`Ce qui a été envoyé PAR le client : ${value} était de type ${typeof value} d'un point de vue JS`);
+        console.log(`La réponse renvoyée PAR Flask (via return) [${response}] (et traitée par JS avec .responseText) est de type ${typeof response}`);
+        callback(response);
+    });
+    request.send(value);
+}; 
 
 site_brand.addEventListener("click", function (e) {
     chat_zone.insertAdjacentHTML("beforeend",`<p> <strong>GrandPy</strong> : Pourquoi tu appuies sur mon logo, t'es fou ou quoi ? Je suis plus très jeune, c'est fragile ici !!</p>`); //Rajouter un compteur de clicks, et rajouter des phrases en fonction de l'état du compteur
@@ -10,21 +23,17 @@ site_brand.addEventListener("click", function (e) {
 });
 
 form.addEventListener("submit", function (e) {
-    
     var message_val = chat_input.value;
-    chat_zone.insertAdjacentHTML("beforeend", `<p class='message'> <strong>Vous</strong> : <span> </span> </p>`); // Ah, pou sûr, il ne supporte pas l'attribution d'attributs avec des variables...
-    chat_zone.querySelector(".message:last-child span").setAttribute("id", `${count}`);
-    document.getElementById(`${count}`).textContent += message_val;
-    count++;
+    chat_zone.insertAdjacentHTML("beforeend", `<p class='message'> <strong>Vous</strong> : <span> </span> </p>`); 
+    chat_zone.querySelector(".message:last-child span").setAttribute("id", `${count_you}`);
+    document.getElementById(`${count_you}`).textContent += message_val;
+    ajaxTest("POST", "/test", message_val, function (response) { //message_val dans du json
+        chat_zone.insertAdjacentHTML("beforeend", `<p class='message'> <strong>GrandPy</strong> : <span> </span> </p>`);
+        chat_zone.querySelector(".message:last-child span").setAttribute("id", `${count_gp}`);
+        document.getElementById(`${count_gp}`).textContent += response;
+        count_gp--;
+    });
+    count_you++;
     chat_input.value = ""; 
     e.preventDefault();
 });
-
-/*
-
-Tests :
-e>e<
-e>e<eeee> (prob)
-e\>e\<eeee\> (prob) C'est coupé parce qu'il le prend pour une balise... avec les <>
-
-*/
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect # , url_for
+from flask import Flask, render_template, redirect, request # , url_for
 from app.forms import Form
+import json
 
 app = Flask("app") #ou __name__ vu que __name__ == "app"
 app.config.from_object("config") 
@@ -7,17 +8,18 @@ app.config.from_object("config")
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Form()
-    if form.validate_on_submit():
-        if form.textarea.data == "bonjour":
-            return redirect('/bonjour')
-        else:
-            return redirect('/erreur')
     return render_template("page.html", form=form)
 
-@app.route('/bonjour')
-def bonjour():
-    return "bonjour"
+#Qu'est-ce que j'essaie de faire ? 
 
-@app.route('/erreur')
-def erreur():
-    return "je ne comprends pas"
+# Il faut écrire ici qqch qui peut gérer ce que le front-end lui aurait envoyé. 
+#Comment gérer une requete POST avec flask
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == "POST":
+        request_data_str = request.data.decode("utf-8") #Du bytes converti en str
+        if "bonjour" in request_data_str.split() or "Bonjour" in request_data_str.split():
+            return "Bonjour"
+        else:
+            return "Désolé, je ne comprends rien d'autre que bonjour ou Bonjour... Et oui je suis borné moi :)"
