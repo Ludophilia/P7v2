8726235e22011a9499d8bcd2af503694fee470a8
Ludophilia <c1q4n93@outlook.com>
2019-05-30 19:49:20 +0200

    Correction erreurs deploiement

---
+++ config.py
@@ -0,0 +1,7 @@
+# To generate a new secret key:
+# import random, string
+# "".join([random.choice(string.printable) for _ in range(24)])
+
+ENV = "development"
+SECRET_KEY = '!2g=Ny*jw:@4;\t;70C1s,()\x0b'
+API_KEY = "AIzaSyDEGAzO7QR0WpWcALuqu4FjhZQLUxkw0kc"
